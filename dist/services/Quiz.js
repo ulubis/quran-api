9c9e03566d0f7a0941f725aaab71e94295d99f7d
ulubis <053214566@ecampus.ut.ac.id>
2024-02-25 15:54:50 +0700

    lb

--- dist/services/Quiz.js
+++ dist/services/Quiz.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.QuizHandler = void 0;
 const number_1 = require("../utils/number");
-const array_1 = require("../utils/array");
 class QuizHandler {
 }
 exports.QuizHandler = QuizHandler;
@@ -11,28 +10,9 @@ QuizHandler.surahName = (req, res) => {
     try {
         const index = (0, number_1.getRandomNumber)(1, 114);
         let surah = require(`../../data/quran/${index}.json`);
-        const question = `Dalam Al-Qur'an, surah ${surah.name.transliteration.id} adalah surah ke ....`;
-        const choices = [
-            {
-                choice: index,
-                right: true
-            },
-            {
-                choice: (0, number_1.recursiveRandNumb)(1, 114, index),
-                right: false
-            },
-            {
-                choice: (0, number_1.recursiveRandNumb)(1, 114, index),
-                right: false
-            },
-            {
-                choice: (0, number_1.recursiveRandNumb)(1, 114, index),
-                right: false
-            }
-        ];
         res.send({
-            question: question,
-            choices: (0, array_1.shuffleArray)(choices)
+            question: surah.name.transliteration.id,
+            answer: index
         });
     }
     catch (e) {
